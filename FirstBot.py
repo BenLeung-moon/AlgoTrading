66f4d3d78bff6e8fe32accf0058c2f17c3119c83
Homura <70994374+BenLeung-moon@users.noreply.github.com>
2023-08-24 17:43:05 +1000

    improvement on class

--- FirstBot.py
+++ FirstBot.py
@@ -21,7 +21,6 @@ class FirstBot(Agent):
     """
     Attributes:
         threshold: an int used to store profit margin
-        _holdings: list used to store finished trade units and price
 
     """
 
@@ -32,7 +31,6 @@ class FirstBot(Agent):
         self.private_market = None
         self._waiting_for_server = True
         self.threshold = THRESHOLD
-        self._holdings = []
         self.averageHoldingPrice = 0
 
     def initialised(self):
@@ -47,25 +45,24 @@ class FirstBot(Agent):
     # function used to response to existed order
     def respond_order(self, order: Order):
         new_order = copy.copy(order)
-        new_order.order_side = -order.order_side
+        if order.order_side == OrderSide.BUY:
+            new_order.order_side = OrderSide.SELL
+        else:
+            new_order.order_side = OrderSide.BUY
+
         super().send_order(new_order)
         self._waiting_for_server = False
 
     # function used to place order with one time input
     def place_order(self, orderSide: OrderSide, orderType: OrderType,
-                    price: int, units: int, ref: str, isPrivate: bool, owner_or_target: str):
-        if isPrivate:
-            new_order = Order.create_new(self.private_market)
-        else:
-            new_order = Order.create_new(self.public_market)
+                    price: int, units: int, ref: str):
 
+        new_order = Order.create_new(self.public_market)
         new_order.order_side = orderSide
         new_order.order_type = orderType
         new_order.price = price
         new_order.units = units
         new_order.ref = ref
-        if isPrivate:
-            new_order.owner_or_target = owner_or_target
         super().send_order(new_order)
         self._waiting_for_server = False
 
@@ -81,7 +78,7 @@ class FirstBot(Agent):
         for _id, asset in holdings.assets.items():
             self.inform(f"Asset {asset.market}: settled {asset.units}")
             self.inform(f"Assets {asset.market}: Available {asset.units_available}")
-            self.averageHoldingPrice += holdings.cash / asset.units
+            self.averageHoldingPrice = self.holdings.cash_available / asset.units_available
 
     def received_orders(self, orders: List[Order]):
         # pass
@@ -107,39 +104,41 @@ class FirstBot(Agent):
             # response to public/private market
             if order.market == self.public_market and self._waiting_for_server:
                 if order.order_side == OrderSide.BUY:
-                    # sell the stock higher than the bought price
-                    # can be improved as a function later
-                    for i in self._holdings:
-                        if order.price - self.threshold > i[1]:
-                            self.respond_order(order)
+                    if order.price - self.threshold > self.averageHoldingPrice:
+                        self.respond_order(order)
 
                 if order.order_side == OrderSide.SELL:
                     if order.price + self.threshold < self.averageHoldingPrice:
                         self.respond_order(order)
 
-            if order.market == self.private_market:
+            if order.market == self.private_market and self._waiting_for_server \
+                    and not order.mine:
                 if order.order_side == OrderSide.BUY:
                     self.respond_order(order)
                     self.inform(f"Accept buy order with {order.units} units at ${order.price}")
 
                 if order.order_side == OrderSide.SELL:
                     self.respond_order(order)
-                    self._holdings[order_id] = order.price
                     self.inform(f"Accept sell order with {order.units} units at ${order.price}")
-                    self._holdings.append([order.units, order.price])
 
             # cancel order if didn't be traded for a period
+            # can replace on the proactive
             if order.mine and \
                     (datetime.datetime.now().time() >
-                     (order.date_created + datetime.timedelta(seconds=CANCEL_TIME_SECOND)).time()):
+                     (order.date_created + datetime.timedelta(seconds=CANCEL_TIME_SECOND)).time())\
+                    and not order.has_traded:
                 self.cancel_order(order)
                 self._waiting_for_server = True
 
         #  calculate spread and check whether liquid the orders
         spread = lowest_sell - highest_buy
-        if spread <= 10:
-            self.place_order(OrderSide.SELL, OrderType.LIMIT, highest_buy+5,
-                             2, "liquidity", False, "NA")
+        if spread <= 30 and self._waiting_for_server and self.markets == self.public_market:
+            self.place_order(OrderSide.SELL, OrderType.LIMIT, highest_buy + 5,
+                             2, "liquidity")
+
+        if spread > 100 and self._waiting_for_server and self.markets == self.public_market:
+            self.place_order(OrderSide.SELL, OrderType.LIMIT, lowest_sell - 100,
+                             2, "liquidity")
 
     def order_accepted(self, order: Order):
         self.inform(f"Order accepted: {order.ref}")
@@ -155,7 +154,7 @@ class FirstBot(Agent):
 
     def pre_start_tasks(self):
         # #can call any method in the class periodically
-        # self.execute_periodically()
+        self.execute_periodically(self.received_holdings(), 1)
         pass
 
 
